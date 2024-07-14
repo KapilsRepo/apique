a06aaf891015c02dea3b4caf7c47fd7a1f607494
Anand Kalyankar <anand.kalyankar@gmail.com>
2024-07-14 12:22:06 +0530

    switched to the develop branch and loaded spring boot boilerplate

---
+++ src/main/java/com/obapi/apique/ApiqueApplication.java
@@ -0,0 +1,13 @@
+package com.obapi.apique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obapi/apique/ApiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.obapi.apique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
