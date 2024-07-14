1094998b7e510ef9476400263bd840915ea6e954
Anand Kalyankar <anand.kalyankar@gmail.com>
2024-07-14 15:04:22 +0530

    worked on h2 setup and application properties

--- src/main/java/com/obapi/apique/ApiqueApplication.java
+++ src/main/java/com/obapi/apique/ApiqueApplication.java
@@ -2,12 +2,16 @@ package com.obapi.apique;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
 
 @SpringBootApplication
 public class ApiqueApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiqueApplication.class, args);
+		System.out.println("Apique Application Started");
+
 	}
 
 }
