af4b1e7ee06bd199e7a69f70bce248d6d2e82efc
Anand Kalyankar <anand.kalyankar@gmail.com>
2024-07-18 19:15:40 +0530

    setup bootstrap css and static css

--- src/main/java/com/obapi/apique/configuration/SecurityConfig.java
+++ src/main/java/com/obapi/apique/configuration/SecurityConfig.java
@@ -2,8 +2,11 @@ package com.obapi.apique.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Configuration
@@ -12,5 +15,12 @@ public class SecurityConfig {
     @Bean
     public WebSecurityCustomizer webSecurityCustomizer() {
         return (web) -> web.ignoring().requestMatchers(new AntPathRequestMatcher("/h2-console/**"));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+       http
+               .formLogin(Customizer.withDefaults());
+        return http.build();
     }
 }
--- src/main/java/com/obapi/apique/controller/EndpointController.java
+++ src/main/java/com/obapi/apique/controller/EndpointController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/index")
+@RequestMapping(value = "/")
 public class EndpointController {
-    @GetMapping
+    @GetMapping(value = "index")
     public String index() {
         return "index";
     }
